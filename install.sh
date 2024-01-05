8cc7fb4b2ce5c05d324486f7dee1d2c9d8f966aa
Red4DR <2121100617@soy.utj.edu.mx>
2024-01-04 22:24:24 -0600

    Se agrega compatibilidad para distintas distros.

--- install.sh
+++ install.sh
@@ -50,6 +50,51 @@ function ctrl_c() {
 
 trap ctrl_c INT
 
+function get_distro() {
+	distro=$(grep -m1 "^ID=" /etc/os-release | awk -F'=' '{print $2}' | tr -d '"')
+	distro_like=$(grep -m1 "^ID_LIKE=" /etc/os-release | awk -F'=' '{print $2}' | tr -d '"')
+
+	case "$distro" in
+	"debian" | "ubuntu" | "linuxmint" | "kali")
+		echo "apt"
+		;;
+	"fedora" | "centos" | "rhel")
+		echo "dnf"
+		;;
+	"arch" | "manjaro" | *arch*)
+		echo "pacman"
+		;;
+	*)
+		if [[ $distro_like == *arch* ]]; then
+			echo "pacman"
+		else
+		echo "No se pudo determinar el gestor de paquetes.\nPor favor, instala los siguientes paquetes manualmente:\npython3\npython3-pip\npython3-venv\nwget"
+		fi
+		;;
+	esac
+}
+
+package_handler=$(get_distro)
+
+declare -A dependencies=(
+	["python3"]="sudo $package_handler install python-is-python3"
+	["pip"]="sudo $package_handler install python3-pip"
+	["venv"]="sudo $package_handler install python3-venv"
+	["wget"]="sudo $package_handler install wget"
+)
+
+for dependency in "${!dependencies[@]}"; do
+	if ! command -v "$dependency" &>/dev/null; then
+		echo -e "${yellow}Instalando $dependency...${reset}"
+		eval "${dependencies[$dependency]}" # Install the dependency
+		if [ $? -ne 0 ]; then
+			echo -e "${red}No se pudo instalar $dependency.${reset}"
+			exit 1
+		fi
+		echo -e "${green}$dependency instalado.${reset}"
+	fi
+done
+
 cd "$agoracli_path" || exit # Change directory to $HOME/.local/bin/agoracli. If the directory doesnt exist, exit the script.
 declare -A files_to_download=(
 	["agoracli.py"]="https://raw.githubusercontent.com/4DRIAN0RTIZ/AgoraCLI/master/src/agoracli.py"
